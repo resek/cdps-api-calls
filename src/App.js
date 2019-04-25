a1299226c4aee494d2e035306d88dd4eea6e5e02
resek <matija.resek@gmail.com>
2019-04-25 11:02:32 +0200

    database bug solved

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
 
   state = {
     spinner: true,
+    rows: null,
     getUrls: [],
     allObjects: []
   }
@@ -16,9 +17,9 @@ class App extends Component {
       .then(response => {
 
         const sortedByInk = response.data.sort((a, b) => b.ink - a.ink);
-        const first500 = sortedByInk.slice(0, 500);
-        
-        first500.map(cdp => {
+        const first1000 = sortedByInk.slice(0, 1000);
+
+        first1000.map(cdp => {
           return this.state.getUrls.push(axios.get(`https://mkr.tools/api/v1/cdp/${cdp.id}/actions`));
         });
       })
@@ -37,7 +38,13 @@ class App extends Component {
               method: "put",
               data: this.state.allObjects
             })
-            this.setState({spinner: false});
+            .then(response => {
+              console.log(response);
+              this.setState({spinner: false, rows: response.data.length});
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
           })
           .catch(function (error) {
             console.log(error);
@@ -53,7 +60,10 @@ class App extends Component {
     return (
       <div className="App">
         {this.state.spinner ? <CircularProgress size={80} style={{marginTop: 160}}/>
-        : <div style={{marginTop: 160}}>https://cdps-api-calls.firebaseio.com/cdps-api-calls.json</div>} 
+        : <div style={{marginTop: 160}}>
+          <div>https://cdps-api-calls.firebaseio.com/cdps-api-calls.json</div>
+          <div>Rows created: {this.state.rows}</div>
+        </div>} 
       </div>
     );
   }
