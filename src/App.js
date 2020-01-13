bcd16081cd5ec1db1d8d0f672cdc8739d319fe91
resek <matija.resek@gmail.com>
2020-01-13 19:09:37 +0100

    IDs of cdps in array (100)

--- src/App.js
+++ src/App.js
@@ -9,10 +9,110 @@ class App extends Component {
     spinner: true,
     rows: null,
     getUrls: [],
+    ids: [3088,
+      153543,
+      12826,
+      1238,
+      14357,
+      13917,
+      99779,
+      5144,
+      5422,
+      3647,
+      4004,
+      2554,
+      4166,
+      5141,
+      51,
+      17074,
+      16707,
+      2283,
+      16671,
+      4865,
+      784,
+      15860,
+      16515,
+      1287,
+      5156,
+      14960,
+      16930,
+      1481,
+      2442,
+      4225,
+      16932,
+      2709,
+      119434,
+      16978,
+      356,
+      135,
+      153642,
+      2544,
+      12890,
+      2873,
+      2035,
+      19487,
+      153658,
+      14869,
+      153914,
+      4224,
+      15036,
+      681,
+      4265,
+      19304,
+      4222,
+      4223,
+      19357,
+      603,
+      14829,
+      2214,
+      5447,
+      4969,
+      16298,
+      18883,
+      4537,
+      340,
+      19064,
+      14863,
+      18774,
+      17,
+      5858,
+      5000,
+      2507,
+      14142,
+      3969,
+      19328,
+      14430,
+      86670,
+      17859,
+      705,
+      5459,
+      14788,
+      4895,
+      15858,
+      14691,
+      18316,
+      3873,
+      1776,
+      153570,
+      147225,
+      127744,
+      15471,
+      16014,
+      147121,
+      2075,
+      44086,
+      14975,
+      16592,
+      14852,
+      18285,
+      5162,
+      3351,
+      4580,
+      17285],
     allObjects: []
   }
 
-  componentDidMount() {
+  /* componentDidMount() {
     axios.get('https://mkr.tools/api/v1/cdps')
       .then(response => {
 
@@ -58,6 +158,42 @@ class App extends Component {
       .catch(function (error) {
         console.log(error);
       });
+  } */
+
+  componentDidMount() {
+
+  this.state.ids.forEach(id => {
+    return this.state.getUrls.push(axios.get(`https://mkr.tools/api/v1/cdp/${id}/actions`));
+  });
+
+  axios.all(this.state.getUrls)
+    .then(response => {
+      console.log("response from all calls");
+      console.log(response);
+      response.map(cdp => {
+        return cdp.data.forEach(row => {
+          this.state.allObjects.push(row);
+        })
+      })
+    })
+    .then(() => {
+      console.log("saving to DB")
+      axios({
+        url: "https://cdps-api-calls.firebaseio.com/cdps-api-calls.json",
+        method: "put",
+        data: this.state.allObjects
+      })
+      .then(response => {
+        console.log(response);
+        this.setState({spinner: false, rows: response.data.length});
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 
   render() {
